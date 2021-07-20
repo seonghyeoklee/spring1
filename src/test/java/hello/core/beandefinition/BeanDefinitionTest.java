816f9b79c17b04aaf48797279d2adb230a1a998e
seonghyeoklee <dltjdgur327@gmail.com>
2021-07-20 23:58:35 +0900

    Modify BeanDefinitionTest

--- src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
+++ src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -9,8 +9,8 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class BeanDefinitionTest {
 
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-//    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+//    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+    GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
 
     @Test
     @DisplayName("빈 설정 메타정보 확인")
