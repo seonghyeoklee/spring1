e2fb2cbdbdf24260477f01aaa7bc10777ee93e9b
seonghyeoklee <dltjdgur327@gmail.com>
2021-07-31 17:17:57 +0900

    Edit the comments in springContainer

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -41,12 +41,14 @@ public class SingletonTest {
     @Test
     @DisplayName("스프링 컨테이너와 싱글톤")
     void springContainer() {
+
+        //스프링 컨테이너에서 빈 조회하도록 수정
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberService memberService1 = ac.getBean("memberService", MemberService.class);
         MemberService memberService2 = ac.getBean("memberService", MemberService.class);
 
-        //참조값이 다른 것을 확인
+        //참조값이 같은 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
