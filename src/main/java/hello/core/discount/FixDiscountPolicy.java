9c6ff1fa547ee2a941b02a6e7498a15f9b3ec41d
seonghyeoklee <dltjdgur327@gmail.com>
2021-09-14 23:59:34 +0900

    @Primary 우선권 제공

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary
 public class FixDiscountPolicy implements DiscountPolicy{
 
     private int discountFixAmount = 1000;   //1000원 할인
